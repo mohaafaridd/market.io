bd4abaa79accc2486a5f3dc2a612cdc91e578865
Mohammed <moha.khamis@outlook.com>
2019-08-22 19:51:20 +0200

    created product model

---
+++ backend/models/product.model.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const schema = new Schema({
+  category: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 36
+  },
+
+  manufacturer: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 36
+  },
+
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 36
+  },
+
+  description: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 36
+  },
+
+  model: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 36
+  },
+
+  color: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 36
+  }
+});
+
+const Product = model('Product', schema);
+
+module.exports = Product;
