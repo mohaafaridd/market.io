c17d96f640de93d59eb1b003d5e297888e606d2a
Mohammed <moha.khamis@outlook.com>
2019-08-23 01:59:27 +0200

    created shop model

---
+++ models/shop.model.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require('mongoose');
+const { isEmail } = require('validator');
+
+const schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 15,
+    match: /^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/,
+  },
+
+  username: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 15,
+    match: /^[a-zA-Z]+$/,
+  },
+
+  phone: [
+    {
+      type: String,
+      required: true,
+      match: /^01(\d{9})$/,
+    },
+  ],
+
+  email: [
+    {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      validate(value) {
+        if (!isEmail(value)) {
+          throw new Error('Invalid Email');
+        }
+      },
+    },
+  ],
+
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlength: 100,
+  },
+});
+
+const Shop = model('Shop', schema);
+
+module.exports = Shop;
