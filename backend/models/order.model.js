f595d677d6e15693e23466594ec0d6f12c73f4bb
Mohammed <moha.khamis@outlook.com>
2019-08-22 20:01:49 +0200

    created order model

---
+++ backend/models/order.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+const { ObjectId } = Schema.Types;
+
+const schema = new Schema({
+  delivered: {
+    type: Boolean,
+    required: true
+  },
+
+  customer: {
+    type: ObjectId,
+    required: true
+  },
+
+  products: [
+    {
+      type: ObjectId
+    }
+  ]
+});
+
+const Order = model('Order', schema);
+
+module.exports = Order;
