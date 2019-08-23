549476e3e27d7b4b7037609f965bc1d50e8b6402
Mohammed <moha.khamis@outlook.com>
2019-08-23 15:27:05 +0200

    more option added to testing user registration

---
+++ tests/fixtures/db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user.model');
+
+const userOneId = new mongoose.Types.ObjectId();
+const userOne = {
+  _id: userOneId,
+  firstname: 'Mohammed',
+  lastname: 'Farid',
+  email: 'mohammed@gmail.com',
+  phone: '01012227424',
+  password: '123456',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.SECRET_KEY),
+    },
+  ],
+};
+
+const userTwoId = new mongoose.Types.ObjectId();
+const userTwo = {
+  _id: userTwoId,
+  firstname: 'Sherif',
+  lastname: 'Ashraf',
+  email: 'sherif@gmail.com',
+  phone: '01252186752',
+  password: '654321',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, process.env.SECRET_KEY),
+    },
+  ],
+};
+
+const setupDatabase = async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+  await new User(userTwo).save();
+};
+
+module.exports = {
+  userOneId,
+  userOne,
+  userTwoId,
+  userTwo,
+  setupDatabase,
+};
--- tests/user.test.js
+++ tests/user.test.js
@@ -2,17 +2,29 @@ const request = require('supertest');
 const app = require('../app');
 const User = require('../models/user.model');
 
+const {
+  setupDatabase,
+  userOne,
+  userOneId,
+  userTwo,
+  userTwoId,
+} = require('./fixtures/db');
+
+beforeEach(setupDatabase);
+
 test('User Registration', async () => {
+  const defaultUser = {
+    firstname: 'Mohammed',
+    lastname: 'Adel',
+    email: 'adel@gmail.com',
+    phone: '01124597562',
+    password: '5791355',
+  };
+
   const response = await request(app)
     .post('/user/register')
     .send({
-      user: {
-        firstname: 'Mohammed',
-        lastname: 'Farid',
-        email: 'Mohammed@gmail.com',
-        phone: '01012227424',
-        password: '123456',
-      },
+      user: defaultUser,
     })
     .expect(201);
 
@@ -20,11 +32,14 @@ test('User Registration', async () => {
   expect(user).not.toBeNull();
   expect(response.body).toMatchObject({
     user: {
-      firstname: 'Mohammed',
-      lastname: 'Farid',
-      email: 'mohammed@gmail.com',
-      phone: '01012227424',
-      password: '123456',
+      firstname: defaultUser.firstname,
+      lastname: defaultUser.lastname,
+      email: defaultUser.email,
+      phone: defaultUser.phone,
     },
+
+    token: user.tokens[0].token,
   });
+
+  expect(user.password).not.toBe(defaultUser.password);
 });
