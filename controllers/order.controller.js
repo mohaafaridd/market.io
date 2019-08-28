27b058353f8d0bf4b827e22a0a39e26a1bdc8559
Mohammed <moha.khamis@outlook.com>
2019-08-29 01:03:55 +0200

    request to post an order

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,7 +1,16 @@
 const Order = require('../models/order.model');
+const Cart = require('../models/cart.model');
 
 const postOrder = async (req, res) => {
   try {
+    const { products: productsIds } = req.body;
+
+    const requests = productsIds.map(id =>
+      Cart.findOne({ owner: req.user._id, id })
+    );
+    const carts = await Promise.all(requests);
+
+    res.json({ carts });
   } catch (error) {
     res.json({ error: { ...error }, message: error.message });
   }
