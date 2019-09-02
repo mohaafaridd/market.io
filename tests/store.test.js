161cb3867e874e6b9e5ca1bc0fd71a1360714300
Mohammed <moha.khamis@outlook.com>
2019-09-02 16:32:54 +0200

    improved get store test

--- tests/store.test.js
+++ tests/store.test.js
@@ -1,6 +1,12 @@
 const request = require('supertest');
 const app = require('../app');
-const { setupDatabase, storeOne, productOne } = require('./fixtures/db');
+const Role = require('../middlewares/role');
+const {
+  setupDatabase,
+  storeOneId,
+  storeOne,
+  productOne,
+} = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 
@@ -8,4 +14,23 @@ test('gets a store data', async () => {
   const response = await request(app)
     .get(`/store/${storeOne.username}`)
     .expect(200);
+
+  expect(response.body).toMatchObject({
+    store: {
+      role: Role.Store,
+      name: storeOne.name,
+      username: storeOne.username,
+      email: storeOne.email,
+      phone: storeOne.phone,
+    },
+
+    products: [
+      {
+        category: productOne.category,
+        manufacturer: productOne.manufacturer,
+        name: productOne.name,
+        store: storeOneId.toHexString(),
+      },
+    ],
+  });
 });
