a465818b83335835825ce75358c84ccbc44e9544
Mohammed <moha.khamis@outlook.com>
2019-09-02 17:42:38 +0200

    created courier model

--- models/courier.model.js
+++ models/courier.model.js
@@ -3,16 +3,15 @@ const uniqueValidator = require('mongoose-unique-validator');
 const { isEmail } = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const role = require('../middlewares/role');
 
-const schema = new Schema({
-  // User & Courier
+const Schema = new Schema({
   firstname: {
     type: String,
     minlength: 2,
     maxlength: 15,
     trim: true,
     match: /^[a-zA-Z]+$/,
+    required: true,
   },
 
   lastname: {
@@ -21,30 +20,9 @@ const schema = new Schema({
     maxlength: 15,
     trim: true,
     match: /^[a-zA-Z]+$/,
+    required: true,
   },
 
-  // Store
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 15,
-    match: /^[a-zA-Z]+$/,
-  },
-
-  username: {
-    type: String,
-    minlength: 2,
-    maxlength: 15,
-    match: /^[a-zA-Z]+$/,
-  },
-
-  // Courier
-  workplace: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-  },
-
-  // General
   phone: {
     type: String,
     required: true,
@@ -64,16 +42,20 @@ const schema = new Schema({
     },
   },
 
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-    maxlength: 100,
+  workplace: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
   },
 
   role: {
     type: String,
     required: true,
+  },
+
+  rating: {
+    type: Number,
+    required: true,
+    default: 10,
   },
 
   tokens: [
@@ -88,71 +70,57 @@ const schema = new Schema({
 
 schema.plugin(uniqueValidator);
 
-// Getting user orders
-schema.virtual('cart', {
-  ref: 'Cart',
-  localField: '_id',
-  foreignField: 'owner',
-});
-
-// Getting user orders
 schema.virtual('orders', {
   ref: 'Order',
   localField: '_id',
-  foreignField: 'customer',
+  foreignField: 'courier',
 });
 
-// Getting user products if role is store
-schema.virtual('products', {
-  ref: 'Product',
-  localField: '_id',
-  foreignField: 'store',
-});
 // Hashing plain text password
 schema.pre('save', async function preSave(next) {
-  const user = this;
-  if (user.isModified('password')) {
-    user.password = await bcrypt.hash(user.password, 10);
+  const courier = this;
+  if (courier.isModified('password')) {
+    courier.password = await bcrypt.hash(courier.password, 10);
   }
   next();
 });
 
 schema.methods.generateAuthToken = async function generateAuthToken() {
-  const user = this;
+  const courier = this;
   const token = jwt.sign(
-    { id: user.id.toString(), role: user.role },
+    { id: courier.id.toString(), role: courier.role },
     process.env.SECRET_KEY
   );
-  user.tokens = user.tokens.concat({ token });
-  await user.save();
+  courier.tokens = courier.tokens.concat({ token });
+  await courier.save();
   return token;
 };
 
 // removing password and tokens from response
 schema.methods.toJSON = function toJSON() {
-  const user = this;
-  const userObject = user.toObject();
+  const courier = this;
+  const courierObject = courier.toObject();
 
-  delete userObject.password;
-  delete userObject.tokens;
+  delete courierObject.password;
+  delete courierObject.tokens;
 
-  return userObject;
+  return courierObject;
 };
 
 schema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({ email });
-  if (!user) {
+  const courier = await Courier.findOne({ email });
+  if (!courier) {
     throw new Error('Unable to login!');
   }
 
-  const isMatch = await bcrypt.compare(password, user.password);
+  const isMatch = await bcrypt.compare(password, courier.password);
   if (!isMatch) {
     throw new Error('Unable to login!');
   }
 
-  return user;
+  return courier;
 };
 
-const User = model('User', schema);
+const Courier = model('Courier', schema);
 
-module.exports = User;
+module.exports = Courier;
