4c8a9d4319d1a062c1df1190e95afc92327f3cd2
Mohammed <moha.khamis@outlook.com>
2019-09-06 16:34:34 +0200

    changed find query to aggregate

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,15 +1,33 @@
 const Product = require('../models/product.model');
 
 const getProducts = async (req, res) => {
-  const { name, minimum = 0, maximum = 1000000 } = req.query;
+  const { category, name, minimum = 0, maximum = 1000000 } = req.query;
 
-  const products = await Product.find(
+  if (minimum > maximum) {
+    return res
+      .status(400)
+      .json({ message: "failed, minimum can't be more than maximum" });
+  }
+
+  const products = await Product.aggregate([
     {
-      $text: { $search: name },
-      price: { $gte: minimum, $lte: maximum },
+      $match: {
+        $and: [
+          name ? { $text: { $search: name } } : { name: { $exists: true } },
+          { price: { $gte: parseFloat(minimum), $lte: parseFloat(maximum) } },
+          { category: category ? category : { $exists: true } },
+        ],
+      },
     },
-    { score: { $meta: 'textScore' } }
-  ).sort({ score: { $meta: 'textScore' } });
+  ]);
+  // Working
+  // const products = await Product.find(
+  //   {
+  //     $text: { $search: name },
+  //     price: { $gte: minimum, $lte: maximum },
+  //   },
+  //   { score: { $meta: 'textScore' } }
+  // ).sort({ score: { $meta: 'textScore' } });
 
   res.json({ products });
 };
