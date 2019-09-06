45cf890f561669a9de5aa35c42faf3bee802cda4
Mohammed <moha.khamis@outlook.com>
2019-09-06 17:07:58 +0200

    added count, limit and skip pages to the query

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,35 +1,37 @@
 const Product = require('../models/product.model');
 
 const getProducts = async (req, res) => {
-  const { category, name, minimum = 0, maximum = 1000000 } = req.query;
+  const { category, name, minimum = 0, maximum = 1000000, page } = req.query;
 
   if (minimum > maximum) {
     return res
       .status(400)
-      .json({ message: "failed, minimum can't be more than maximum" });
+      .json({ message: "minimum can't be more than maximum" });
   }
 
-  const products = await Product.aggregate([
-    {
-      $match: {
-        $and: [
-          name ? { $text: { $search: name } } : { name: { $exists: true } },
-          { price: { $gte: parseFloat(minimum), $lte: parseFloat(maximum) } },
-          { category: category ? category : { $exists: true } },
-        ],
+  const matchQuery = {
+    $and: [
+      name ? { $text: { $search: name } } : { name: { $exists: true } },
+      {
+        price: {
+          $gte: parseFloat(minimum),
+          $lte: parseFloat(maximum),
+        },
       },
-    },
-  ]);
-  // Working
-  // const products = await Product.find(
-  //   {
-  //     $text: { $search: name },
-  //     price: { $gte: minimum, $lte: maximum },
-  //   },
-  //   { score: { $meta: 'textScore' } }
-  // ).sort({ score: { $meta: 'textScore' } });
+      { category: category ? category : { $exists: true } },
+    ],
+  };
 
-  res.json({ products });
+  const products = await Product.find(matchQuery, {
+    score: { $meta: 'textScore' },
+  })
+    .sort({ score: { $meta: 'textScore' } })
+    .limit(10)
+    .skip(page ? page - 1 : 0);
+
+  const count = await Product.find(matchQuery).countDocuments();
+
+  res.json({ products, count });
 };
 
 module.exports = { getProducts };
