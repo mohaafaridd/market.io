f757509a748aaf889cbce4a982dddaa646763d56
Mohammed <moha.khamis@outlook.com>
2019-09-08 05:49:52 +0200

    added test to check for patch product as admin

--- tests/admin.test.js
+++ tests/admin.test.js
@@ -3,8 +3,38 @@ const app = require('../app');
 
 const Product = require('../models/product.model');
 
-const { setupDatabase, adminOne, adminOneId } = require('./fixtures/db');
+const {
+  adminOne,
+  adminOneId,
+  productOne,
+  productOneId,
+  setupDatabase,
+  storeOneId,
+} = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 
-test('Should edit product as an administrator', async () => {});
+test('Should edit product as an administrator', async () => {
+  const response = await request(app)
+    .patch(`/products/api/${productOneId}`)
+    .set('Authorization', `Bearer ${adminOne.tokens[0].token}`)
+    .send({
+      updates: {
+        name: 'S10+',
+        amount: 20,
+        discount: 10,
+      },
+    })
+    .expect(200);
+
+  expect(response.body).toMatchObject({
+    product: {
+      ...productOne,
+      name: 'S10+',
+      amount: 20,
+      discount: 10,
+      _id: productOneId.toHexString(),
+      store: storeOneId.toHexString(),
+    },
+  });
+});
