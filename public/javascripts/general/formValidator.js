1245bc7450abb12899a400a490a06bfb2e0e213e
Mohammed <moha.khamis@outlook.com>
2019-09-12 08:15:16 +0200

    change keys in formValidators to fit backend

--- public/javascripts/general/formValidator.js
+++ public/javascripts/general/formValidator.js
@@ -6,10 +6,10 @@ export const clearErrors = () => {
 };
 
 export const displayError = error => {
-  const className = '.' + error.field;
+  const className = '.' + error.path;
   const field = document.querySelector('.error' + className);
   if (field) {
-    field.innerHTML = error.reason;
+    field.innerHTML = error.kind;
   }
 };
 
@@ -47,85 +47,85 @@ const validateName = name => {
   name = name.trim();
 
   if (!name.length) {
-    return { success: false, reason: 'required', field: 'name' };
+    return { success: false, kind: 'required', path: 'name' };
   }
 
   if (name.length > 20) {
-    return { success: false, reason: 'long', field: 'name' };
+    return { success: false, kind: 'long', path: 'name' };
   }
 
   if (name.length < 2) {
-    return { success: false, reason: 'short', field: 'name' };
+    return { success: false, kind: 'short', path: 'name' };
   }
 
-  return { success: true, field: 'name' };
+  return { success: true, path: 'name' };
 };
 
 const validatePhone = phone => {
   phone = phone.trim();
 
   if (!phone.length) {
-    return { success: false, reason: 'required', field: 'phone' };
+    return { success: false, kind: 'required', path: 'phone' };
   }
 
   if (phone.length !== 11) {
-    return { success: false, reason: 'length', field: 'phone' };
+    return { success: false, kind: 'length', path: 'phone' };
   }
 
   const regex = RegExp(/^01(\d{9})$/);
   if (!regex.test(phone)) {
-    return { success: false, reason: 'regex', field: 'phone' };
+    return { success: false, kind: 'regex', path: 'phone' };
   }
 
-  return { success: true, field: 'phone' };
+  return { success: true, path: 'phone' };
 };
 
 const validateEmail = email => {
   if (!email.length) {
-    return { success: false, reason: 'required', field: 'email' };
+    return { success: false, kind: 'required', path: 'email' };
   }
 
   if (!isEmail(email)) {
-    return { success: false, reason: 'format', field: 'email' };
+    return { success: false, kind: 'format', path: 'email' };
   }
 
-  return { success: true, field: 'email' };
+  return { success: true, path: 'email' };
 };
 
 const validatePassword = password => {
   password = password.trim();
 
   if (!password.length) {
-    return { success: false, reason: 'required', field: 'password' };
+    return { success: false, kind: 'required', path: 'password' };
   }
 
   if (password.length > 100) {
-    return { success: false, reason: 'long', field: 'password' };
+    return { success: false, kind: 'long', path: 'password' };
   }
 
   if (password.length < 6) {
-    return { success: false, reason: 'short', field: 'password' };
+    return { success: false, kind: 'short', path: 'password' };
   }
 
-  return { success: true, field: 'password' };
+  return { success: true, path: 'password' };
 };
 
 const validateRePassword = (password, repassword) => {
   if (!repassword.length) {
-    return { success: false, reason: 'required', field: 'repassword' };
+    return { success: false, kind: 'required', path: 'repassword' };
   }
 
   if (repassword.length > 100) {
-    return { success: false, reason: 'long', field: 'repassword' };
+    return { success: false, kind: 'long', path: 'repassword' };
   }
 
   if (repassword.length < 6) {
-    return { success: false, reason: 'short', field: 'repassword' };
+    return { success: false, kind: 'short', path: 'repassword' };
   }
 
   if (password !== repassword) {
-    return { success: false, reason: 'match', field: 'repassword' };
+    return { success: false, kind: 'match', path: 'repassword' };
   }
 
-  return { success: true, field: 'repassword' };
+  return { success: true, path: 'repassword' };
 };
