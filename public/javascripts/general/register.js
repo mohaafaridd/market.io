6cb319acee1141ab0b61181858defe069bf051b8
Mohammed <moha.khamis@outlook.com>
2019-09-12 05:20:45 +0200

    random registration information

--- public/javascripts/general/register.js
+++ public/javascripts/general/register.js
@@ -1,26 +1,29 @@
 import axios from 'axios';
+import faker from 'faker';
 
-const registerBtn = document.getElementById('register-submit-button');
+if (document.title === 'Register') {
+  const registerBtn = document.getElementById('register-submit-button');
 
-registerBtn.addEventListener('click', async e => {
-  e.preventDefault();
+  registerBtn.addEventListener('click', async e => {
+    e.preventDefault();
 
-  try {
-    const response = await axios({
-      method: 'POST',
-      url: '/users/api/register',
-      data: {
-        user: {
-          firstname: 'Mohammed',
-          lastname: 'Farid',
-          email: 'Mohammed@gmail.com',
-          phone: '01012227',
-          password: '123456',
+    try {
+      const response = await axios({
+        method: 'POST',
+        url: '/users/api/register',
+        data: {
+          user: {
+            firstname: faker.name.firstName(),
+            lastname: faker.name.lastName(),
+            email: faker.internet.email(),
+            phone: faker.phone.phoneNumber('010########'),
+            password: faker.internet.password(10),
+          },
         },
-      },
-    });
-    console.log('response :', response.data);
-  } catch (error) {
-    console.log(error.response.data);
-  }
-});
+      });
+      console.log('response :', response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  });
+}
