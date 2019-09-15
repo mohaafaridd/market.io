e35449f1b122fa726b7750021aae7a5509c8d57d
Mohammed <moha.khamis@outlook.com>
2019-09-15 09:45:27 +0200

    removed all repetitive auth files

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const authorization = require('../middlewares/store.authorization');
-const authentication = require('../middlewares/store.authentication');
+const authorization = require('../middlewares/authorization');
+const authentication = require('../middlewares/authentication');
 const Role = require('../middlewares/role');
 
 const api = require('./api/products.api');
@@ -9,7 +9,7 @@ const router = express.Router();
 router.use('/api', api);
 router.get('/add', authorization(Role.Store), authentication, (req, res) => {
   try {
-    const { store } = req;
+    const { client: store } = req;
     res.render('products/add-product', { title: 'Add Product', store });
   } catch (error) {
     res.redirect('/');
