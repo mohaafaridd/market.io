a09d8c3f8e958737030cd621aa3507f1b18a7c9b
Mohammed <moha.khamis@outlook.com>
2019-09-20 11:29:42 +0200

    removed the ability to set amount, not required

--- public/javascripts/products/add-to-cart.js
+++ public/javascripts/products/add-to-cart.js
@@ -6,8 +6,10 @@ const addToCartButtons = document.getElementsByClassName('add-to-cart-btn');
 for (const cartBtn of addToCartButtons) {
   cartBtn.addEventListener('click', async e => {
     e.preventDefault();
+    cartBtn.disabled = true;
+
     const { parentElement: form } = cartBtn;
-    const fields = ['product-id', 'store-id', 'amount'];
+    const fields = ['product-id', 'store-id'];
 
     // This block is not for humans
     const elements = [...form.elements]
@@ -24,19 +26,20 @@ for (const cartBtn of addToCartButtons) {
 
     const product = elements['product-id'];
     const store = elements['store-id'];
-    const amount = elements['amount'];
 
     try {
       const response = await axios.post('/carts/api', {
         product,
         store,
-        amount: parseInt(amount),
+        amount: 1,
       });
 
       console.log('response.data :', response.data);
     } catch (error) {
       console.log('error.response :', error.response);
     }
+
+    cartBtn.disabled = false;
   });
 }
 
