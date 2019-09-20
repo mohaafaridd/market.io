d252dff62be46c981d9d77ae44da00e59ce76c9e
Mohammed <moha.khamis@outlook.com>
2019-09-20 13:36:44 +0200

    wrapped the search div with form tag

--- public/javascripts/products/add-to-cart.js
+++ public/javascripts/products/add-to-cart.js
@@ -42,25 +42,3 @@ for (const cartBtn of addToCartButtons) {
     cartBtn.disabled = false;
   });
 }
-
-// if (addToCartBtn) {
-//   addToCartBtn.addEventListener('click', async e => {
-//     e.preventDefault();
-
-//     const product = document.getElementById('product-id').value;
-//     const store = document.getElementById('store-id').value;
-//     const amount = document.getElementById('amount').value;
-
-//     try {
-//       const response = await axios.post('/carts/api', {
-//         product,
-//         store,
-//         amount,
-//       });
-
-//       console.log('response.data :', response.data);
-//     } catch (error) {
-//       console.log('error.response :', error.response);
-//     }
-//   });
-// }
