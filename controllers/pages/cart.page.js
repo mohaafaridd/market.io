8055744f87290d582a5b37c505ee0ae594a46bf6
Mohammed <moha.khamis@outlook.com>
2019-10-04 19:45:00 +0200

    simple aggregation for get cart

--- controllers/pages/cart.page.js
+++ controllers/pages/cart.page.js
@@ -1,8 +1,51 @@
-const getCart = async (req, res) => {
-  const { cart, bill, client } = req;
-  const { role } = client;
+const Cart = require('../../models/cart.model');
 
-  res.render('user/cart', { title: 'Shopping Cart', [role]: true, cart, bill });
+const getCart = async (req, res) => {
+  const { client: user } = req;
+  const { role } = user;
+  const cart = await Cart.aggregate([
+    { $match: { user: user._id, ordered: false } },
+    {
+      $lookup: {
+        from: 'products',
+        localField: 'product',
+        foreignField: '_id',
+        as: 'product',
+      },
+    },
+
+    {
+      $unwind: {
+        path: '$product',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+
+    {
+      $lookup: {
+        from: 'bundles',
+        localField: 'bundle',
+        foreignField: '_id',
+        as: 'bundle',
+      },
+    },
+
+    {
+      $unwind: {
+        path: '$bundle',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+
+    {
+      $group: {
+        _id: '$_id',
+      },
+    },
+  ]);
+  console.log('cart :', cart);
+
+  res.render('user/cart', { title: 'Shopping Cart', [role]: true, cart });
 };
 
 module.exports = {
