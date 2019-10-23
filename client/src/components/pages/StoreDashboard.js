44f59653775f7969999aaf2253c4c898bc2c3edc
Mohammed <moha.khamis@outlook.com>
2019-10-23 07:37:54 +0200

    added route protection to store dashboard

--- client/src/components/pages/StoreDashboard.js
+++ client/src/components/pages/StoreDashboard.js
@@ -1,10 +1,19 @@
-import React, { Fragment } from 'react';
-import { Link, Route } from 'react-router-dom';
+import React, { Fragment, useContext } from 'react';
+import { Link, Route, useHistory } from 'react-router-dom';
 import StoreNavbar from '../layout/StoreNavbar';
 
+import AuthContext from '../../context/auth/authContext';
 import Statistics from '../store/Statistics';
 import ProductsTab from '../store/products-tab/ProductsTab';
 const Dashboard = ({ match }) => {
+  const authContext = useContext(AuthContext);
+  const { client } = authContext;
+  let history = useHistory();
+
+  if (!client || client.role !== 'Store') {
+    history.push('/');
+  }
+
   return (
     <Fragment>
       <StoreNavbar />
