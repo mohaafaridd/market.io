edb7e7986dbea60d7b5fde9d4a5f1e3fedbfc997
Mohammed <moha.khamis@outlook.com>
2019-10-31 16:21:52 +0200

    created get bundle in get cart aggregation

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -85,193 +85,127 @@ const getCarts = async (req, res) => {
     const { client: user } = req;
     const { role } = user;
 
-    const carts = await Cart.find({ user: user._id })
-      .populate('product')
-      .populate('bundle');
+    // bundles: contained all bundles user has added in his cart
+    const bundles = await Cart.aggregate([
+      // Step 1.0: Fetch all bundles
+      {
+        $match: {
+          user: user._id,
+          ordered: false,
+          bundle: { $exists: true, $ne: null },
+        },
+      },
 
-    // const cart = await Cart.aggregate([
-    //   { $match: { user: user._id, ordered: false } },
+      // Step 2.0: lookup bundles to get products
+      {
+        $lookup: {
+          from: 'bundles',
+          localField: 'bundle',
+          foreignField: '_id',
+          as: 'bundle',
+        },
+      },
 
-    //   // Step 1.1: Look for the bundle
-    //   {
-    //     $lookup: {
-    //       from: 'bundles',
-    //       localField: 'bundle',
-    //       foreignField: '_id',
-    //       as: 'bundle',
-    //     },
-    //   },
+      // Step 3.0: Uncompress and delete Empty Bundles (Not possible in the first place)
+      {
+        $unwind: {
+          path: '$bundle',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
 
-    //   // Step 1.2: Clear if no bundle
-    //   {
-    //     $unwind: {
-    //       path: '$bundle',
-    //       preserveNullAndEmptyArrays: true,
-    //     },
-    //   },
+      // Step 4.0: lookup for offers to get products
+      {
+        $lookup: {
+          from: 'products',
+          localField: 'bundle.offers.product',
+          foreignField: '_id',
+          as: 'bundle.products',
+        },
+      },
 
-    //   // Step 2.1: Move product id from bundle array to product property
-    //   {
-    //     $project: {
-    //       cart: '$_id',
-    //       bundle: '$bundle',
-    //       amount: '$amount',
-    //       product: {
-    //         $ifNull: ['$product', '$bundle.products.product'],
-    //       },
-    //     },
-    //   },
+      // This query came from https://stackoverflow.com/a/47994330/11781188 for reference
+      // Step 5.0: Merge products and their discount offers
+      {
+        $addFields: {
+          products: {
+            // Maps the offers to get their product id and offer discount and merge it with the original product
+            $map: {
+              input: '$bundle.offers',
+              in: {
+                $mergeObjects: [
+                  {
+                    $arrayElemAt: [
+                      '$bundle.products',
+                      {
+                        $indexOfArray: [
+                          '$bundle.products._id',
+                          '$$this.product',
+                        ],
+                      },
+                    ],
+                  },
+                  '$$this',
+                ],
+              },
+            },
+          },
+        },
+      },
 
-    //   // Step 2.2: Separate the array if exists
-    //   {
-    //     $unwind: {
-    //       path: '$product',
-    //       preserveNullAndEmptyArrays: true,
-    //     },
-    //   },
+      // This query loops over the products array to calculate the proper discount on the bundle's products
+      // Step 6.0: Calculate bill
+      {
+        $project: {
+          products: '$products',
+          store: '$store',
+          bill: {
+            $sum: {
+              $map: {
+                input: '$products',
+                as: 'product',
+                in: {
+                  $multiply: [
+                    '$$product.price',
+                    '$amount',
+                    // discount = 10%
+                    // then it's 0.9 of the price
+                    // ((100 - 10)/100)
+                    {
+                      $divide: [
+                        { $subtract: [100, '$$product.discount'] },
+                        100,
+                      ],
+                    },
+                  ],
+                },
+              },
+            },
+          },
+        },
+      },
 
-    //   // Step 3.1: Look for product
-    //   {
-    //     $lookup: {
-    //       from: 'products',
-    //       localField: 'product',
-    //       foreignField: '_id',
-    //       as: 'product',
-    //     },
-    //   },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'store',
+          foreignField: '_id',
+          as: 'store',
+        },
+      },
 
-    //   // Step 3.2: Moves the project out of the array
-    //   {
-    //     $unwind: {
-    //       path: '$product',
-    //       preserveNullAndEmptyArrays: true,
-    //     },
-    //   },
-
-    //   // Step 4.1: Get product store
-    //   {
-    //     $lookup: {
-    //       from: 'stores',
-    //       localField: 'product.store',
-    //       foreignField: '_id',
-    //       as: 'store',
-    //     },
-    //   },
-
-    //   // Step 4.2: Move store out of the array
-    //   {
-    //     $unwind: {
-    //       path: '$store',
-    //       preserveNullAndEmptyArrays: true,
-    //     },
-    //   },
-
-    //   // Step 5.1: Get product details from bundle (TO EXTRACT DISCOUNT)
-    //   {
-    //     $project: {
-    //       amount: '$amount',
-    //       bundle: '$bundle',
-    //       cart: '$cart',
-    //       product: '$product',
-    //       store: '$store',
-    //       details: {
-    //         $filter: {
-    //           input: '$bundle.products',
-    //           as: 'cart',
-    //           cond: { $eq: ['$product._id', '$$cart.product'] },
-    //         },
-    //       },
-    //     },
-    //   },
-
-    //   // Step 5.2: Add discount property
-    //   {
-    //     $project: {
-    //       amount: '$amount',
-    //       bundle: '$bundle',
-    //       cart: '$cart',
-    //       product: '$product',
-    //       store: '$store',
-    //       discount: { $ifNull: ['$details.discount', '$product.discount'] },
-    //     },
-    //   },
-
-    //   // Step 5.3: Extract array element
-    //   {
-    //     $unwind: {
-    //       path: '$discount',
-    //       preserveNullAndEmptyArrays: true,
-    //     },
-    //   },
-
-    //   // Step 5.4: Change discount into fraction
-    //   {
-    //     $project: {
-    //       amount: '$amount',
-    //       bundle: '$bundle',
-    //       cart: '$cart',
-    //       discount: '$discount',
-    //       product: '$product',
-    //       store: '$store',
-    //       saved: {
-    //         $divide: ['$discount', 100],
-    //       },
-    //       fraction: {
-    //         $subtract: [
-    //           1,
-    //           {
-    //             $divide: ['$discount', 100],
-    //           },
-    //         ],
-    //       },
-    //     },
-    //   },
-
-    //   // Step 6: Project to calculate Bill
-    //   {
-    //     $project: {
-    //       amount: '$amount',
-    //       bundle: '$bundle',
-    //       cart: '$cart',
-    //       discount: '$discount',
-    //       product: '$product',
-    //       store: '$store',
-    //       saved: { $multiply: ['$amount', '$product.price', '$saved'] },
-    //       bill: { $multiply: ['$amount', '$product.price', '$fraction'] },
-    //     },
-    //   },
-
-    //   // Step 7: Group all products within their bundles
-    //   {
-    //     $group: {
-    //       _id: '$bundle._id',
-    //       amount: { $first: '$amount' },
-    //       bill: { $sum: '$bill' },
-    //       bundle: { $first: '$bundle' },
-    //       cart: { $first: '$cart' },
-    //       saved: { $sum: '$saved' },
-    //       products: {
-    //         $push: {
-    //           cart: '$cart',
-    //           product: '$product',
-    //           store: '$store',
-    //           bundle: '$bundle',
-    //           amount: '$amount',
-    //           bill: '$bill',
-    //           discount: '$discount',
-    //           saved: '$saved',
-    //         },
-    //       },
-    //     },
-    //   },
-    // ]);
-
-    // const bill = cart.reduce((a, b) => a + b.bill, 0);
+      {
+        $unwind: {
+          path: '$store',
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+    ]);
 
     res.json({
       success: true,
       message: 'Carts found!',
-      carts,
+      bundles,
     });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
