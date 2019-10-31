1af487da851fed4297bc625312bb3d3d7af690dd
Mohammed <moha.khamis@outlook.com>
2019-10-31 16:26:08 +0200

    moved cart aggregation into a helper

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -2,6 +2,8 @@ const Cart = require('../models/cart.model');
 const Product = require('../models/product.model');
 const Bundle = require('../models/bundle.model');
 
+const helper = require('./helpers/cart');
+
 // @route       POST api/carts
 // @desc        Create a cart
 // @access      Private
@@ -86,121 +88,7 @@ const getCarts = async (req, res) => {
     const { role } = user;
 
     // bundles: contained all bundles user has added in his cart
-    const bundles = await Cart.aggregate([
-      // Step 1.0: Fetch all bundles
-      {
-        $match: {
-          user: user._id,
-          ordered: false,
-          bundle: { $exists: true, $ne: null },
-        },
-      },
-
-      // Step 2.0: lookup bundles to get products
-      {
-        $lookup: {
-          from: 'bundles',
-          localField: 'bundle',
-          foreignField: '_id',
-          as: 'bundle',
-        },
-      },
-
-      // Step 3.0: Uncompress and delete Empty Bundles (Not possible in the first place)
-      {
-        $unwind: {
-          path: '$bundle',
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-
-      // Step 4.0: lookup for offers to get products
-      {
-        $lookup: {
-          from: 'products',
-          localField: 'bundle.offers.product',
-          foreignField: '_id',
-          as: 'bundle.products',
-        },
-      },
-
-      // This query came from https://stackoverflow.com/a/47994330/11781188 for reference
-      // Step 5.0: Merge products and their discount offers
-      {
-        $addFields: {
-          products: {
-            // Maps the offers to get their product id and offer discount and merge it with the original product
-            $map: {
-              input: '$bundle.offers',
-              in: {
-                $mergeObjects: [
-                  {
-                    $arrayElemAt: [
-                      '$bundle.products',
-                      {
-                        $indexOfArray: [
-                          '$bundle.products._id',
-                          '$$this.product',
-                        ],
-                      },
-                    ],
-                  },
-                  '$$this',
-                ],
-              },
-            },
-          },
-        },
-      },
-
-      // This query loops over the products array to calculate the proper discount on the bundle's products
-      // Step 6.0: Calculate bill
-      {
-        $project: {
-          products: '$products',
-          store: '$store',
-          bill: {
-            $sum: {
-              $map: {
-                input: '$products',
-                as: 'product',
-                in: {
-                  $multiply: [
-                    '$$product.price',
-                    '$amount',
-                    // discount = 10%
-                    // then it's 0.9 of the price
-                    // ((100 - 10)/100)
-                    {
-                      $divide: [
-                        { $subtract: [100, '$$product.discount'] },
-                        100,
-                      ],
-                    },
-                  ],
-                },
-              },
-            },
-          },
-        },
-      },
-
-      {
-        $lookup: {
-          from: 'users',
-          localField: 'store',
-          foreignField: '_id',
-          as: 'store',
-        },
-      },
-
-      {
-        $unwind: {
-          path: '$store',
-          preserveNullAndEmptyArrays: true,
-        },
-      },
-    ]);
+    const bundles = await helper.getBundles(user);
 
     res.json({
       success: true,
