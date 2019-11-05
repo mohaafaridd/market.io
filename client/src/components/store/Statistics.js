dea974dbcd66f3cbae05ff419b875a0c7483967b
Mohammed <moha.khamis@outlook.com>
2019-11-05 15:44:50 +0200

    added store context into statistics component

--- client/src/components/store/Statistics.js
+++ client/src/components/store/Statistics.js
@@ -1,6 +1,15 @@
-import React from 'react';
-
+import React, { useContext, useEffect } from 'react';
+import StoreContext from '../../context/store/storeContext';
 const Statistics = () => {
+  const { getStatistics, statistics, loading } = useContext(StoreContext);
+
+  useEffect(() => {
+    getStatistics();
+  }, []);
+
+  if (loading) {
+    return <h4>Loading</h4>;
+  }
   return (
     <div>
       <h1>Stats</h1>
--- client/src/context/store/storeReducer.js
+++ client/src/context/store/storeReducer.js
@@ -3,7 +3,11 @@ import { GET_STATISTICS } from '../types';
 export default (state, action) => {
   switch (action.type) {
     case GET_STATISTICS:
-      return { ...state, current: action.payload.statistics, loading: false };
+      return {
+        ...state,
+        statistics: action.payload.statistics[0],
+        loading: false,
+      };
 
     default:
       return state;
