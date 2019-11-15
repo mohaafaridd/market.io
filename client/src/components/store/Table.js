513dc707ad74a6238bb3e503f24271e4ffbaffca
Mohammed <moha.khamis@outlook.com>
2019-11-16 00:36:03 +0200

    added sorting to react tables

--- client/src/components/store/Table.js
+++ client/src/components/store/Table.js
@@ -22,7 +22,12 @@ const Table = ({ columns, data }) => {
 					{headerGroups.map(headerGroup => (
 						<tr {...headerGroup.getHeaderGroupProps()}>
 							{headerGroup.headers.map(column => (
-								<th {...column.getHeaderProps()}>{column.render('Header')}</th>
+								<th {...column.getHeaderProps(column.getSortByToggleProps())}>
+									<span className='select-none'>{column.render('Header')}</span>
+									<span className='text-white select-none'>
+										{column.isSorted ? (column.isSortedDesc ? ' ▼' : ' ▲') : ''}
+									</span>
+								</th>
 							))}
 						</tr>
 					))}
