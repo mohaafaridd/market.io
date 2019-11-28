134dfc08c5d7bcff43487ad4482ad88e2b72bf94
Mohammed <moha.khamis@outlook.com>
2019-11-28 16:16:32 +0200

    enabled add to cart button

--- client/src/components/search/ProductCard.js
+++ client/src/components/search/ProductCard.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import numeral from 'numeral';
 
+import AuthContext from '../../context/auth/authContext';
+import UserContext from '../../context/user/userContext';
+
 const ProductCard = ({ product }) => {
+	const { client } = useContext(AuthContext);
+	const { addCart } = useContext(UserContext);
 	const { name, description, price, discount } = product;
 	const discounted = (1 - discount / 100) * price;
 	const image = Buffer.from(product.image.data).toString('base64');
@@ -21,20 +26,24 @@ const ProductCard = ({ product }) => {
 						<p className='price'>{numeral(discounted).format('$0,0.00')}</p>
 					)}
 				</section>
+				{client && client.role === 'User' && (
+					<section className='user-action'>
+						<button
+							className='btn btn-primary'
+							onClick={e => addCart(product, 'product')}
+						>
+							<i className='fas fa-shopping-cart mr-2'></i>
+							Cart
+						</button>
+						<button className='btn btn-danger'>
+							<i className='fas fa-heart mr-2'></i>
+							Wishlist
+						</button>
 
-				<section className='user-action'>
-					<button className='btn btn-primary'>
-						<i className='fas fa-shopping-cart mr-2'></i>
-						Cart
-					</button>
-					<button className='btn btn-danger'>
-						<i className='fas fa-heart mr-2'></i>
-						Wishlist
-					</button>
-
-					{/* Planned: Favorites */}
-					{/* dis-planned: Bundle */}
-				</section>
+						{/* Planned: Favorites */}
+						{/* dis-planned: Bundle */}
+					</section>
+				)}
 			</figure>
 		</li>
 	);
