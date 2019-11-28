c80b3bfdc028783d5fb0ea4b2e96a8f0c84b51b3
Mohammed <moha.khamis@outlook.com>
2019-11-28 14:20:11 +0200

    proper products results styling

--- client/src/components/search/ProductCard.js
+++ client/src/components/search/ProductCard.js
@@ -2,29 +2,41 @@ import React from 'react';
 import numeral from 'numeral';
 
 const ProductCard = ({ product }) => {
-	const { name, description, price } = product;
+	const { name, description, price, discount } = product;
+	const discounted = (1 - discount / 100) * price;
 	const image = Buffer.from(product.image.data).toString('base64');
+	console.log('product', product);
+
 	return (
-		<li className='tile product'>
-			<img src={`data:image/jpeg;base64,${image}`} alt={`${name}`} />
-			<h5 className='text-xs'>{name}</h5>
+		<li className='product-container'>
+			<figure className='tile product'>
+				<img src={`data:image/jpeg;base64,${image}`} alt={`${name}`} />
+				<h5 className='title'>{name}</h5>
 
-			<p className='text-gray-500'>Price</p>
-			<p>{numeral(price).format('$0,0.00')}</p>
+				<section className='prices'>
+					<p className={`${discount > 0 ? 'discounted-price' : 'price'}`}>
+						{numeral(price).format('$0,0.00')}
+					</p>
 
-			<section className='user-action'>
-				<button className='btn btn-primary'>
-					<i className='fas fa-shopping-cart mr-2'></i>
-					Cart
-				</button>
-				<button className='btn btn-danger'>
-					<i className='fas fa-heart mr-2'></i>
-					Wishlist
-				</button>
+					{discount > 0 && (
+						<p className='price'>{numeral(discounted).format('$0,0.00')}</p>
+					)}
+				</section>
 
-				{/* Planned: Favorites */}
-				{/* dis-planned: Bundle */}
-			</section>
+				<section className='user-action'>
+					<button className='btn btn-primary'>
+						<i className='fas fa-shopping-cart mr-2'></i>
+						Cart
+					</button>
+					<button className='btn btn-danger'>
+						<i className='fas fa-heart mr-2'></i>
+						Wishlist
+					</button>
+
+					{/* Planned: Favorites */}
+					{/* dis-planned: Bundle */}
+				</section>
+			</figure>
 		</li>
 	);
 };
